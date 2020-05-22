598ca2e3083c3f969b95f402111957c941d9753d
Ben <hzcly98@163.com>
2020-05-22 09:51:40 +0800

    Initial Commit

---
+++ BBChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BBChat
+//
+//  Created by Ben on 2020/5/22.
+//  Copyright © 2020 Benben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
